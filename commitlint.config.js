92006298bf3569d0306ea6098289bbac6f4acfa5
Unai Gonzalez <unaigoe91@gmail.com>
2026-02-25 09:03:00 +0100

    fix: allow uppercase in commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -21,7 +21,6 @@ export default {
     'subject-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
     'header-max-length': [2, 'always', 100]
   }
